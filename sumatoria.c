dae3373975174112c001dfb9f96fa7bc4fa6a4c1
José Andrés Lucatero Tenorio <benjamin.wwe@hotmail.es>
2017-07-02 22:48:22 -0500

    Add files via upload

---
+++ sumatoria.c
@@ -0,0 +1,24 @@
+#include <stdio.h)
+
+int main()
+{
+	int n,n1,n2,sum;
+	sum=0;
+	printf("Dame un numero: ");
+	scanf("%i",&n1);
+	printf("Dame otro numero: ");
+	scanf("%i",&n2);
+	if(n1==n2||n1>n2)
+		printf("El segundo numero debe de ser mayor al primero");
+	else
+	{
+		for(n=n1;n<n2;n++)
+		{
+			sum=sum+n;
+			printf("%i+",n);
+		}
+		sum=sum+n;
+		printf("%i",n);
+		printf("=%i",sum);
+	}
+	return 0;
